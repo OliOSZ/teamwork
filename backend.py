110fafc88405a3df3e550e0b679ea87a1c9d63cf
OliOSZ <146973447+OliOSZ@users.noreply.github.com>
2024-09-24 11:59:08 +0200

    Outline
    lage outline av backend for kalk

---
+++ backend.py
@@ -0,0 +1,13 @@
+def leggSammen():
+    tall1 = input("Skriv in første tallet: ")
+    tall2 = input("Skriv in andre tallet: ")
+    sum = int(tall1) + int(tall2)
+    print(f'{tall1} + {tall2} = {sum}')
+    
+    
+def trekkFra():
+    tall1 = input("Skriv in første tallet: ")
+    tall2 = input("Skriv in andre tallet: ")
+    diff = int(tall1) - int(tall2)
+    print(f'{tall1} - {tall2} = {diff}')
+    
